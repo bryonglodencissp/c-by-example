9f2cb53059118d21a6a37b01d44502e1bde65a44
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2016-03-07 22:37:04 +0000

    io: Do buffered io instead of single character

--- io/io.c
+++ io/io.c
@@ -2,15 +2,15 @@
 
 xg_ssize_t xg_io_copy(struct xg_io_writer *dst, struct xg_io_reader *src)
 {
-	uint8_t b;
+	uint8_t b[256];
 
 	if (!dst->write) {
-		while (src->read(&b, 1) >= 1)
+		while (src->read(b, sizeof(b)) >= 1)
 			;
 	}
 
-	while (src->read(&b, 1) >= 1) {
-		if (dst->write(&b, 1) != 1)
+	while (src->read(b, sizeof(b)) >= sizeof(b)) {
+		if (dst->write(b, sizeof(b)) < sizeof(b))
 			return 0;
 	}
 
