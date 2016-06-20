f49dcd52a008f75115b82448e63fd8dbe46a0e9f
Jerry Jacobs <jerry.jacobs@dualinventive.com>
2016-06-20 12:02:11 +0200

    Implement reflection in only one file

--- reflection/reflect.c
+++ reflection/reflect.c
@@ -20,7 +20,167 @@
 #include <string.h>
 #include <inttypes.h>
 
-#include "reflect.h"
+#define stringify(x) #x
+
+uint32_t generateHash(const char *string, size_t len)
+{
+	uint32_t hash = 0;
+	for(size_t i = 0; i < len; ++i)
+		hash = 65599 * hash + string[i];
+	return hash ^ (hash >> 16);
+}
+
+#define DECL_STRUCT_FINGERPRINT \
+	struct { \
+		uint32_t id; \
+		const char *name; \
+		const char *body; \
+	} __meta;
+
+	struct __meta {
+		uint32_t id;
+		const char *name;
+		const char *body;
+	};
+
+#define DECL_STRUCT_MEMBER(type, name) type name;
+
+#define DEF_STRUCT(sname, body) \
+	static const char *sname##__name = stringify(sname); \
+	static const char *sname##__body = stringify(body); \
+	struct sname { \
+		DECL_STRUCT_FINGERPRINT \
+		body \
+	}; \
+	typedef struct sname sname##_t;
+
+#define decl_var(sname, x) struct sname x; x.__meta.id = generateHash(stringify(sname), sizeof(stringify(s))); x.__meta.name = sname##__name; x.__meta.body = sname##__body;
+#define decl_get_id(x) x.__meta.id
+#define decl_get_name(x) x.__meta.name
+#define decl_get_body(x) x.__meta.body
+
+struct str {
+	const char *str;
+	size_t len;
+};
+
+enum __member_type {
+	MEMBER_TYPE_UNKNOWN  = 0,
+	MEMBER_TYPE_SIZE_T   = 1,
+	MEMBER_TYPE_CHAR_PTR = 2
+};
+
+static const char *__member_type_str[] = {
+	"unknown",
+	"size_t",
+	"char*"
+};
+
+struct __member_meta {
+	size_t id;
+	size_t offset;
+	size_t len;
+	enum __member_type type;
+};
+
+static inline const char *decl_get_type_str(enum __member_type type)
+{
+	return __member_type_str[type];
+}
+
+void decl_get_member_meta(const char *member, const char *__meta_body, struct __member_meta *meta)
+{
+	const char *cur   = __meta_body;
+	const char *token = __meta_body;
+	size_t offset = 0;
+	meta->id = 0;
+	meta->offset = 0;
+	meta->len = 0;
+	meta->type = MEMBER_TYPE_UNKNOWN;
+	enum __member_type type = MEMBER_TYPE_UNKNOWN;
+	struct str mtype = { .str = NULL };
+	struct str mname = { .str = NULL };
+
+	while (*cur) {
+		if (*cur == ';' || *cur == ' ') {
+			size_t len = cur - token;
+			size_t pos;
+			if (token != __meta_body)
+				pos = cur - __meta_body - len;
+			else
+				pos = 0;
+
+			if (len) {
+				// Fetch member type
+				if (!mtype.str) {
+					mtype.str = token;
+					mtype.len = len;
+
+					mname.str = NULL;
+					mname.len = 0;
+
+					if (memcmp(mtype.str, "size_t", mtype.len) == 0) {
+						type = MEMBER_TYPE_SIZE_T;
+						meta->len = sizeof(size_t);
+					} else if (memcmp(mtype.str, "char*", mtype.len) == 0) {
+						type = MEMBER_TYPE_CHAR_PTR;
+						meta->len += sizeof(char *);
+					}
+
+					if (meta->id && mtype.str != __meta_body)
+						offset += meta->len;
+
+					meta->id++;
+				} else {
+					mname.str = token;
+					mname.len = len;
+				}
+
+				// Fetch member name
+				if (mname.str) {
+					if (strlen(member) == mname.len && memcmp(member, mname.str, mname.len) == 0) {
+						meta->type = type;
+						break;
+					}
+					mtype.str = NULL;
+				}
+			}
+			token = cur + 1;
+		}
+		cur++;
+	}
+
+	if (meta->id)
+		meta->offset = offset;
+}
+
+enum __member_type decl_get_type(void *s, const char *member)
+{
+	struct __meta *meta = (struct __meta *)s;
+	struct __member_meta mmeta;
+	decl_get_member_meta(member, meta->body, &mmeta);
+	return mmeta.type;
+}
+
+void decl_print_member(void *s, const char *member)
+{
+	struct __meta *meta = (struct __meta *)s;
+	struct __member_meta mmeta;
+	decl_get_member_meta(member, meta->body, &mmeta);
+
+	if (mmeta.type == MEMBER_TYPE_UNKNOWN) {
+		printf("--- member \"%s\" -> unknown\n", member);
+		return;
+	}
+
+	printf("--- member \"%s\"\n", member);
+	printf("%s offset: %zu\n", member, mmeta.offset);
+	printf("%s len: %zu\n", member, mmeta.len);
+	printf("%s type(%u): \"%s\"\n", member, mmeta.type, decl_get_type_str(mmeta.type));
+	if (mmeta.type == MEMBER_TYPE_SIZE_T)
+		printf("%s: %zu\n", member, *(size_t *)((uintptr_t)s + sizeof(*meta) + mmeta.offset));
+}
+
 
 DEF_STRUCT(coord,
 	DECL_STRUCT_MEMBER(size_t, monkey)
